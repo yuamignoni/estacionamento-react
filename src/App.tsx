f51b3c9e558fb57da666bafc8eb146b1aff90bc7
Yuarã <yuamignoni@gmail.com>
2024-05-19 16:04:31 -0300

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import ListarVagas from "./pages/ListarVagas";
+import CadastroReserva from "./pages/CadastroReserva";
+import { VagasProvider } from "./context/VagasContext";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VagasProvider>
+      <Router>
+        <div className="App">
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<ListarVagas />} />
+              <Route path="/cadastro" element={<CadastroReserva />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </VagasProvider>
   );
 }
 
